1b7165508a63cc98f643ab4aeb71305472c2c58a
Ma <101021254+CodeWithMa@users.noreply.github.com>
2026-07-26 16:25:51 +0200

    Fix lua subfolder loading

--- hyprland.lua
+++ hyprland.lua
@@ -1,4 +1,11 @@
 -- Refer to the wiki for more information.
 -- https://wiki.hypr.land/Configuring/
 
-require("land/*.lua")
+require("land.autostart")
+require("land.environment")
+require("land.input")
+require("land.keybindings")
+require("land.look-and-feel")
+require("land.monitors")
+require("land.permissions")
+require("land.windows-and-workspaces-rules")
